e2ce84a340ff7731db52701169fa236252e92068
franciscolopez18n-ops <francisco.lopez18n@gmail.com>
2025-09-29 10:46:37 +0200

    Add files via upload

--- ej1a.php
+++ ej1a.php
@@ -13,58 +13,31 @@
             th: crea encabezados en esa fila
     -->
     <?php 
-        $impares1 = [];
-        $impares2 = [];
+        $impares = [];
+        $suma = 0;
 
-        bucle_for($impares1);
-        bucle_while($impares2);
-
-        //Para guardar en el array
-        function bucle_for($impares){
-            echo "<h2>Usando un bucle for: </h2><br>";
-            for ($i = 0; $i < 20; $i++) {
+        for ($i = 0; $i < 20; $i++) {
                 $impares[$i] = 2 * $i + 1; //cualquier numero multiplicado por 2 es par entonces le sumamos 1
-            }
-            crea_tabla($impares);
-        }
-        function bucle_while($impares){
-            echo "<h2>Usando un bucle While: </h2><br>";
-            $numero = 0;
-            $i = 0;  
-
-            while (count($impares) < 20) { 
-                $numero++;
-                if ($numero % 2 != 0) { // si es impar
-                    $impares[$i] = $numero;
-                    $i++; 
-                }
-            }
-            crea_tabla($impares);
-        }
-        //Para crear la tabla
-        function crea_tabla($impares){
-            echo "<table border='1'cellspacing='0'>";
-            //creamos los encabezados
-            echo "<tr>
-                    <th>Indice</th>
-                    <th>Valor</th>
-                    <th>Suma</th>
-                 </tr>";
-            //creamos las celdas
-            $suma = 0;
-            for ($i = 0; $i < count($impares); $i++) {
-                $suma += $impares[$i]; 
-                echo "<tr>
-                        <td>$i</td>
-                        <td>$impares[$i]</td>
-                        <td>$suma</td>
-                     </tr>";
-            }
-
-            echo "</table>";
-            echo "<br><br>";
         }
         
+        echo "<table border='1'cellspacing='0'>";
+      
+        echo "<tr>
+                <th>Indice</th>
+                <th>Valor</th>
+                <th>Suma</th>
+             </tr>";
+     
+        for ($i = 0; $i < count($impares); $i++) {
+            $suma += $impares[$i]; 
+            echo "<tr>
+                    <td>$i</td>
+                    <td>$impares[$i]</td>
+                    <td>$suma</td>
+                 </tr>";
+         }
+
+        echo "</table>";      
     ?>
 </body>
 </html>
