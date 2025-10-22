e8ffa2b14db1ee01cd3d30ff80c6e1420aab30e4
franciscolopez18n-ops <francisco.lopez18n@gmail.com>
2025-10-22 09:04:08 +0200

    Add files via upload

---
+++ files/fichero1.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Fichero 1 - Guardar alumnos</title>
+</head>
+<!--Programa fichero1.php: formulario que recoja los datos de alumnos y los almacene un fichero con
+nombre alumnos1.txt (una fila por alumno). Los campos del fichero estarán separados por posiciones:
+Nombre: posición 1 a 40
+Apellido1: posición 41 a 81
+Apellido2: posición 82 a 123
+Fecha Nacimiento: posición 124 a 133
+Localidad: posición 134 a 160
+Las posiciones no ocupadas se completarán con espacios. -->
+<body>
+    <h2>Registro de alumnos</h2>
+
+    <form method="post" action="">
+        <label>Nombre:</label><br>
+        <input type="text" name="nombre" required><br><br>
+
+        <label>Primer Apellido:</label><br>
+        <input type="text" name="apellido1" required><br><br>
+
+        <label>Segundo Apellido:</label><br>
+        <input type="text" name="apellido2" required><br><br>
+
+        <label>Fecha de Nacimiento:</label><br>
+        <input type="date" name="fecha" required><br><br>
+
+        <label>Localidad:</label><br>
+        <input type="text" name="localidad" required><br><br>
+
+        <input type="submit" name="enviar" value="Guardar Alumno">
+    </form>
+
+    <?php
+    if (isset($_POST['enviar'])) {
+        // Ruta del fichero
+        $ruta = "alumnos1.txt";
+
+        // Recoger los datos del formulario
+        $nombre = $_POST['nombre'];
+        $apellido1 = $_POST['apellido1'];
+        $apellido2 = $_POST['apellido2'];
+        $fecha = $_POST['fecha'];
+        $localidad = $_POST['localidad'];
+
+        // Completamos con espacios
+        $nombre = str_pad($nombre, 40);
+        $apellido1 = str_pad($apellido1, 41);
+        $apellido2 = str_pad($apellido2, 42);
+        $fecha = str_pad($fecha, 10);
+        $localidad = str_pad($localidad, 27);
+
+        // Concatenar los campos
+        $linea = $nombre . $apellido1 . $apellido2 . $fecha . $localidad . "\n";
+
+        // Guardardamos en el fichero
+        file_put_contents($ruta, $linea, FILE_APPEND);
+
+        echo "<p>Alumno guardado correctamente en <b>$ruta</b></p>";
+    }
+    ?>
+</body>
+</html>
